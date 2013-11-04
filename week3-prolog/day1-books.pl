db88d62b49254942de7e40904fe5bcb1240c066c
Will Green <will@flux.org.uk>
2013-11-04 14:27:39 +0000

    First day of Prolog from last week.

---
+++ week3-prolog/day1-books.pl
@@ -0,0 +1,21 @@
+%% 7L7W Week 1 - Prolog: Day 1
+%% Books and Their Authors
+%% Will Green - http://flux.org.uk
+
+wrote('Joseph Heller', 'Catch-22').
+wrote('Alastair Reynolds', 'Pushing Ice').
+wrote('Vernor Vinge', 'A Deepness in the Sky').
+wrote('Plato', 'The Republic').
+wrote('Patrick Rothfuss', 'The Name of the Wind').
+wrote('Patrick Rothfuss', 'The Wise Man\'s Fear').
+wrote('Susanna Clarke', 'Jonathan Strange & Mr. Norrell').
+wrote('Douglas Adams', 'The Hitchhiker\'s Guide to the Galaxy').
+wrote('Douglas Adams', 'The Restaurant at the End of the Universe').
+wrote('Douglas Adams', 'Life, the Universe and Everything').
+
+%% Find all books written by an author:
+%% wrote('Douglas Adams', What).
+%% wrote('Patrick Rothfuss', What).
+%% wrote('Susanna Clarke', What).
+
+%% Answer with escape single quotes get the quote repeated, not sure why as yet
---
+++ week3-prolog/day1-musicians.pl
@@ -0,0 +1,46 @@
+%% 7L7W Week 1 - Prolog: Day 1
+%% Musicians, their instruments and genres
+%% Will Green - http://flux.org.uk
+
+plays('Paul McCartney', 'bass').
+plays('John Lennon', 'guitar').
+plays('George Harrison', 'guitar').
+plays('Ringo Starr', 'drums').
+plays('Miles Davis', 'trumpet').
+plays('Duke Ellington', 'piano').
+plays('Rebecca Black', 'voice').
+plays('Ryan Adams', 'guitar').
+plays('Rick Astley', 'voice').
+plays('Paul McCartney', 'voice').
+plays('John Lennon', 'voice').
+plays('George Harrison', 'voice').
+plays('Ringo Starr', 'voice').
+plays('Ryan Adams', 'voice').
+
+genre('Paul McCartney', 'rock').
+genre('John Lennon', 'rock').
+genre('George Harrison', 'rock').
+genre('Ringo Starr', 'rock').
+genre('Miles Davis', 'jazz').
+genre('Duke Ellington', 'jazz').
+genre('Rebecca Black', 'cheese').
+genre('Ryan Adams', 'rock').
+genre('Rick Astley', 'pop').
+
+%% guitar duo, two guitar players who aren't the same person
+guitar_duo(X, Y) :- plays(X, 'guitar'), plays(Y, 'guitar'), \+(X = Y).
+
+%% guitar/singer,  singer and guitar player, can be same person
+guitar_singer(X, Y) :- plays(X, 'guitar'), plays(Y, 'voice').
+
+
+%% Test examples
+
+%% Plays rock guitar
+%% genre(What, 'rock'), plays(What, 'guitar').
+
+%% Possible guitar duos
+%% guitar_duo(X, Y).
+
+%% Possible singer/guitar combinations
+%% guitar_singer(X, Y).
